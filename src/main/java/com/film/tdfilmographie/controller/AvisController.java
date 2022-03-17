4ec4d723793fee0cc63c75462b8a77336cf68b3c
Colas Vincent <60921938+vinctigra@users.noreply.github.com>
2022-03-17 12:35:31 +0100

    add avis

--- src/main/java/com/film/tdfilmographie/controller/AvisController.java
+++ src/main/java/com/film/tdfilmographie/controller/AvisController.java
@@ -1,7 +1,16 @@
 package com.film.tdfilmographie.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/avis")
 public class AvisController {
+
+    @GetMapping
+    public String getAvisList(Model model){
+        return "/avis/add-avis.html";
+    }
 }
